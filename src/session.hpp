8e353396a5134b46fc4b355ea60af0e669ff007f
chengmiao <673094766@qq.com>
2019-03-12 16:28:49 +0800

    11

--- src/session.hpp
+++ src/session.hpp
@@ -33,15 +33,15 @@ public:
             //return MysqlPool::Instance()->getDB(index)->execute(query);
         //});
 
+        auto overload = sol::overload(sol::resolve<gdp::db::DBQuery&(const std::string&)>(&gdp::db::DBQuery::set),
+            sol::resolve<gdp::db::DBQuery&(const std::string&, int)>(&gdp::db::DBQuery::set<int>));
+
         m_luaState->new_usertype<gdp::db::DBQuery>( "DBQuery",
             sol::constructors<gdp::db::DBQuery(const std::string &)>(),
             "insert_into", &gdp::db::DBQuery::insert_into,
             "insert_or_update", &gdp::db::DBQuery::insert_or_update,
             "update", &gdp::db::DBQuery::update,
-            "set", sol::overload(sol::resolve<gdp::db::DBQuery&(const std::string&)>(&gdp::db::DBQuery::set),
-            sol::resolve<gdp::db::DBQuery&(const std::string&, int)>(&gdp::db::DBQuery::set<int>)),
-
-
+            "set", overload,
         );
 
 
