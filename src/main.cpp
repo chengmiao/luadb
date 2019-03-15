353e30332c387ed1337ffe9bb7b6f8391c64d754
chengmiao <673094766@qq.com>
2019-03-15 17:22:41 +0800

    11

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,19 @@ int main(int argc, char* argv[])
 {
     try
     {
-        if (argc != 2)
-        {
-            std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
-            return 1; 
-        }
+        //if (argc != 2)
+        //{
+            //std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
+            //return 1; 
+        //}
 
         // init mysql pool
         MysqlPool::Instance()->init();
 
         // init listen server
         asio::io_context io_context;
-        Server s(io_context, std::atoi(argv[1]));
+        //Server s(io_context, std::atoi(argv[1]));
+        Server s(io_context, 3389);
         io_context.run();
     }
     catch (std::exception& e)
