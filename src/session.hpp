84d3561c7e6757fad09191f3eb9b3555a1984a46
chengmiao <673094766@qq.com>
2019-03-15 17:48:35 +0800

    11

--- src/session.hpp
+++ src/session.hpp
@@ -104,15 +104,13 @@ private:
                 std::string lua_data = std::string(consume_pos(), length);
 
                 int32_t index = 2;
-                MysqlPool::Instance()->getIOContext(index)->post([this, lua_data, index](){
+                //MysqlPool::Instance()->getIOContext(index)->post([this, lua_data, index](){
                     std::cout << "Asio Post" << std::endl;
                     
-                    //m_luaGDb->GetLuaState()->script_file("../src/script/db.lua");
-                    //sol::function lua_on_recv = (*(m_luaGDb->GetLuaState()))["onRecv"];
-                    //lua_on_recv(index, lua_data);
-
-                    m_luaGDb->GetLuaState()->set("tmp", 20);
-                });
+                    m_luaGDb->GetLuaState()->script_file("../src/script/db.lua");
+                    sol::function lua_on_recv = (*(m_luaGDb->GetLuaState()))["onRecv"];
+                    lua_on_recv(index, lua_data);
+                //});
 
                 consume_pos_ += length;
 		    }
